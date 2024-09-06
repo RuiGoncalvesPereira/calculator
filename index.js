a2a506855d7f3aa4eabaca87b0142cbee9cdfed5
Rui Pereira <rui.goncalves@stud.edubs.ch>
2024-09-06 23:54:39 +0200

    Add Buttons and Functions

---
+++ index.js
@@ -0,0 +1,39 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function calculate() {
+    let input = display.value;
+    let result = input.split(/(\+|\-|\*|\/)/) // Operators
+    let antwort = parseFloat(result[0]);
+
+    for (let i = 1; i < result.length; i += 2) {
+        let operator = result[i]; // Operator
+        let nextNum = parseFloat(result[i + 1]); // Next number
+
+        // Perform operation based on the operator
+        switch (operator) {
+            case "+":
+                antwort += nextNum;
+                break;
+            case "-":
+                antwort -= nextNum;
+                break;
+            case "*":
+                antwort *= nextNum;
+                break;
+            case "/":
+                antwort /= nextNum;
+                break;
+        }
+    }
+
+    return display.value = antwort;
+}
+
+
+function clearDisplay() {
+    display.value = "";
+}
